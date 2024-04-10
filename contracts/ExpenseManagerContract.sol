e116ae7f643a65017aacb67729a8157e04897cb9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-10 14:05:28 +0530

    named functions

--- contracts/ExpenseManagerContract.sol
+++ contracts/ExpenseManagerContract.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.0;
 
 contract ExpenseManagerContract {
 
+
+    address public owner;
+
     struct Transaction{
         address  user;
         uint amount;
@@ -12,5 +15,36 @@ contract ExpenseManagerContract {
 
     constructor(){
 
+        owner = msg.sender;
+
     }
+
+    function deposit(){
+
+    }
+
+    function withdraw(){
+
+    }
+
+    function getBalance(){
+
+    }
+
+    function getTransactionsCount(){
+
+    }
+
+    function getTransaction(){
+
+    }
+
+    function getAllTransactions(){
+
+    }
+
+    function changeOwner(){
+
+    }
+
 }
