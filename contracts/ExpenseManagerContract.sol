cf7641c66cd158b1fe98db7a19c15c56656a3504
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-10 23:03:30 +0530

    withdraw function

--- contracts/ExpenseManagerContract.sol
+++ contracts/ExpenseManagerContract.sol
@@ -20,23 +20,27 @@ contract ExpenseManagerContract {
         owner = msg.sender;
 
     }
-    event  Deposit(address indexed _from,uint amount, string _reason, uint _timestamp);
+
+    mapping(address =>uint) public balances;
+
+    event  Depositevent(address indexed _from,uint amount, string _reason, uint _timestamp);
     event  Withdraw(address indexed _to,uint amount, string _reason, uint _timestamp);
 
 
 
     function deposit(uint _amount,string memory _reason)public payable{
         require(_amount >0 ,"Depost amount should be greater than 0");
+        balances[msg.sender]+= amount;
         transactions.push(Transaction(msg.sender,_amount,_reason,block.timestamp));
-        emit Deposit(msg.sender,_amount,_reason,block.timestamp);
-
-
-
-
+        emit Depositevent(msg.sender,_amount,_reason,block.timestamp);
     }
 
-    function withdraw(){
-
+    function withdraw(uint _amount,string memory _reason) public{
+        require(balances[msg.sender]>= _amount,"Insufficent Balance");
+        balances[msg.sender] -= _amount;
+        transactions.push(Transaction(msg.sender,_amount,_reason,block.timestamp));
+        payable(msg.sender).transfer(_amount);
+        emit withdraw(msg.sender,_reason,block.timestamp);
     }
 
     function getBalance(){
